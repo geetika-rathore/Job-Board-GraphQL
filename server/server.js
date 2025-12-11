3d0571eef8567e514b218642482763b39ff0eb43
Geetika Rathore <geetikarathore370@gmail.com>
2025-12-11 12:41:16 +0530

    apollo Server with express

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import { ApolloServerge} from '@apollo/server';
+import { expressMiddleware as apolloMiddleware } from '@as-integrations/express4';
+import cors from 'cors';
+import express from 'express';
+import { authMiddleware, handleLogin } from './auth.js';
+import {readFile} from 'node:fs/promises'
+const PORT = 9000;
+import { resolvers } from './resolvers.js';
+const app = express();
+app.use(cors(), express.json(), authMiddleware);
+
+app.post('/login', handleLogin);
+
+const typeDefs = await readFile('./schema.graphql','utf8')
+
+const apolloServer = new ApolloServer({
+  typeDefs,resolvers
+})
+await apolloServer.start()
+app.use('/graphql',apolloMiddleware(apolloServer))
+app.listen({ port: PORT }, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL is running at: http://localhost:${PORT}/graphql`)
+});
